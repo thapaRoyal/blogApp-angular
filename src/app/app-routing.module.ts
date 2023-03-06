57e7f17cd8a591f81d914decb8be8d675f9d5e8e
thapaRoyal <thaparoyal17@gmail.com>
2023-03-06 18:39:52 +0545

    static routing and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'category',
+    component: SingleCategoryComponent,
+  },
+  {
+    path: 'post',
+    component: SinglePostComponent,
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent,
+  },
+  { 
+    path: 'terms-conditions',
+     component: TermsAndConditionComponent
+  },
+  {
+     path: 'contact',
+      component: ContactUsComponent
+     },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { CommentListComponent } from './comments/comment-list/comment-list.compo
     ContactUsComponent,
     SubscriptionFormComponent,
     CommentFormComponent,
-    CommentListComponent
+    CommentListComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
